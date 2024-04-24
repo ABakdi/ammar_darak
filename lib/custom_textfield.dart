8e0a89390463f6d5063f6e4bf87678ca6d1a4910
ABakdi <abderrahmanebakdi@gmail.com>
2024-04-24 02:57:07 +0100

    converted to form text field

--- lib/custom_textfield.dart
+++ lib/custom_textfield.dart
@@ -3,32 +3,40 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
-  const CustomTextField({super.key, required this.hintText, required this.controller});
+  final String? Function(String?)? validator;
+  const CustomTextField(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      required this.validator});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide:  const BorderSide(color: Colors.transparent, width: 0),
-        ),
-        enabledBorder: OutlineInputBorder(
-           borderRadius: BorderRadius.circular(8),
-          borderSide:  const BorderSide(color: Colors.transparent,
-        ),
-        
-      ),
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-      filled: true,
-      fillColor: const  Color(0xffF5F6FA),
-      hintText: hintText,
-      hintStyle: const TextStyle(
-        fontSize: 14, 
-        fontWeight: FontWeight.w400
-      ),
-    )
-    );
+    return TextFormField(
+        controller: controller,
+        validator: validator,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.transparent, width: 0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+          filled: true,
+          fillColor: const Color(0xffF5F6FA),
+          hintText: hintText,
+          hintStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+        ));
   }
 }
