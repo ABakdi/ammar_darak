57cbd782329ba8cac42ec7b1c0acdc5f30429690
ABakdi <abderrahmanebakdi@gmail.com>
2024-04-24 02:57:19 +0100

    update: user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,35 +2,48 @@ import 'dart:convert';
 
 class User {
   final String id;
-  final String name;
   final String email;
+  final String name;
+  final String username;
+  final bool confirmed;
+  final String createdAt;
+  final String updatedAt;
   final String token;
-  final String password;
-  
+
   User({
     required this.id,
-    required this.name,
     required this.email,
+    required this.name,
+    required this.username,
+    required this.confirmed,
+    required this.createdAt,
+    required this.updatedAt,
     required this.token,
-    required this.password,
   });
 
-  Map<String, dynamic> toMap() {
+  Map<dynamic, dynamic> toMap() {
     return {
-      'name': name,
-      'email': email,
-      'token': token,
-      'password': password,
+      id: id,
+      name: name,
+      email: email,
+      username: username,
+      confirmed: confirmed,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      token: token,
     };
   }
 
-  factory User.fromMap(Map<String, dynamic> map) {
+  factory User.fromMap(Map<dynamic, dynamic> map) {
     return User(
       id: map['_id'] ?? '',
-      name: map['name'] ?? '',
       email: map['email'] ?? '',
+      name: map['name'] ?? '',
+      username: map['username'] ?? '',
+      confirmed: map['confirmed'] ?? false,
+      createdAt: map['createdAt'] ?? '',
+      updatedAt: map['updatedAt'] ?? '',
       token: map['token'] ?? '',
-      password: map['password'] ?? '',
     );
   }
 
