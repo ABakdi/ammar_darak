8830b6dffa1365cbc8a3aa354914af974eabac90
ABakdi <abderrahmanebakdi@gmail.com>
2024-05-17 01:27:51 +0100

    update: added file uri

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String uri = 'http://10.0.2.2:5000';
+  static const String fileUri = 'http://10.0.2.2:9000/ammar-darak/';
+  static const Color kscaffoldColor = Color(0xffffffff);
+  static const Color kcontentColor = Color(0xffF5F5F5);
+  static const Color kprimaryColor = Color(0xffff660e);
 }
